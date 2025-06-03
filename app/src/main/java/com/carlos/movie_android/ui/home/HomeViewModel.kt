a8b71a3e20c3a8d402a31b80bcab03bd2cf37eba
cadumeneses <carloseduardoms646@gmail.com>
2025-06-03 20:04:05 -0300

    feat(home): atualizando home view model

--- app/src/main/java/com/carlos/movie_android/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/carlos/movie_android/ui/home/HomeViewModel.kt
@@ -3,11 +3,19 @@ package com.carlos.movie_android.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.carlos.movie_android.models.MovieModel
+import com.carlos.movie_android.repositories.MovieRepository
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel(private val repository: MovieRepository) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val _movies = MutableLiveData<List<MovieModel>>()
+    val movies: LiveData<List<MovieModel>> = _movies
+
+    init {
+        loadMovies()
     }
-    val text: LiveData<String> = _text
+
+    private fun loadMovies() {
+        _movies.value = repository.getMovies()
+    }
 }
